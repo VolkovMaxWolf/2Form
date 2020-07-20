f681eb6c78c17d016fe2e9b035d80dc8d9f7aafc
VolkovMaxWolf <68462998+VolkovMaxWolf@users.noreply.github.com>
2020-07-20 12:20:21 +0300

    Add files via upload

---
+++ 2Form/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _2Form
+{
+    public partial class Form1 : Form
+    {
+        private const int PIX_IN_ONE = 20;
+        private const int Ox = 8 * PIX_IN_ONE; 
+        private const int Oy = 12 * PIX_IN_ONE; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && Сheck(textBox1.Text) && textBox2.Text != "" && Сheck(textBox2.Text))
+            {
+                Bitmap btm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics graphics = Graphics.FromImage(btm);
+                Pen pen = new Pen(Brushes.Black);
+                Pen pen1 = new Pen(Brushes.DarkGray);
+
+
+                int ver = 0;
+                while (ver < pictureBox1.Width)
+                {
+                    graphics.DrawLine(pen1, ver, 0, ver, pictureBox1.Height);
+                    ver += PIX_IN_ONE;
+                }
+
+                int gor = 0;
+                while (gor < pictureBox1.Height)
+                {
+                    graphics.DrawLine(pen1, 0, gor, pictureBox1.Width, gor);
+                    gor += PIX_IN_ONE;
+                }
+
+                
+                graphics.DrawLine(pen, 8 * PIX_IN_ONE, 0, 8 * PIX_IN_ONE, pictureBox1.Height);
+
+                
+                graphics.DrawLine(pen, 0, 12 * PIX_IN_ONE, pictureBox1.Width, 12 * PIX_IN_ONE);
+                
+                graphics.DrawLine(pen, Ox + 10 * PIX_IN_ONE, Oy, Ox + 10 * PIX_IN_ONE, Oy - 5 * PIX_IN_ONE);
+                
+                graphics.DrawLine(pen, Ox , Oy - 5 * PIX_IN_ONE, Ox + 10 * PIX_IN_ONE, Oy - 5 * PIX_IN_ONE);
+
+                
+
+                int step = 10;
+
+                for (int i = 0; i < 20; i++)
+                {
+                    graphics.DrawLine(pen, Ox + step, Oy, Ox + step, Oy - 5 * PIX_IN_ONE);
+                    step += 10;
+                }
+                step = 10;
+                for (int i = 0; i < 10; i++)
+                {
+                    graphics.DrawLine(pen, Ox, Oy - 5 * PIX_IN_ONE + step, Ox + 10 * PIX_IN_ONE, Oy - 5 * PIX_IN_ONE + step);
+                    step += 10;
+                }
+                graphics.FillEllipse(Brushes.Red, Ox + (Convert.ToInt32(textBox1.Text) * PIX_IN_ONE) - 5, Oy - (Convert.ToInt32(textBox2.Text) * PIX_IN_ONE) - 5, 10, 10);
+
+                textBox3.Text = CheckCoordinates(float.Parse(textBox1.Text), float.Parse(textBox2.Text));
+
+                pictureBox1.Image = btm;
+
+            }
+        }
+        private bool Сheck(String s)
+        {
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!char.IsDigit(arr[i]) && arr[i] != '-')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private String CheckCoordinates(float x, float y)
+        {
+            if (x > 0 && x < 10 && y > 0 && y < 5) return "Да";
+            else if (x < 0 || x > 10 || y < 0 || y > 5) return "Нет";
+            else return "На границе";
+        }
+    }
+}
---
+++ 2Form/Form2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2Form
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            GetNumbers();
+            ShowNumbers();
+
+        }
+
+        private void FindMaxSpeed(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 1 && textBox1.Text != "" && char.IsLetter(Convert.ToChar(textBox1.Text)))
+            {
+                textBox2.Text = Convert.ToString(GetMaxSpeed
+                    (Convert.ToChar(textBox1.Text)));
+            }
+        }
+        private bool Сheck(String s)
+        {
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!char.IsDigit(arr[i]) && arr[i] == '-' && arr[i] != ',')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private int GetMaxSpeed(char ch)
+        {
+            switch (ch)
+            {
+                case 'a': return 300;
+                case 'в': return 45;
+                case 'м': return 245;
+                case 'с': return 1200;
+                case 'п': return 450;
+                default: return 0;
+            }
+        }
+        private void GetNumbers()
+        {
+
+            for (int i = 10; i < 100; i++)
+            {
+                if (Math.Abs(i % 10 - (i - i % 10) / 10) <= 1)
+                textBox3.AppendText(i + " ");
+            }
+            textBox3.AppendText(Environment.NewLine);
+
+
+            int j = 9;
+            while (j < 100)
+            {
+                j++;
+                if (Math.Abs(j % 10 - (j - j % 10) / 10) <= 1)
+                    textBox3.AppendText(j + " ");
+            }
+            textBox3.AppendText(Environment.NewLine);
+
+
+            int k = 10;
+            do
+            {
+                if (Math.Abs(k % 10 - (k - k % 10) / 10) <= 1)
+                    textBox3.AppendText(k + " ");
+                k++;
+            }
+            while (k < 100);
+            textBox3.AppendText(Environment.NewLine);
+        }
+        private void ShowNumbers()
+        {
+            int first = 9;
+            int second = 4;
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    textBox4.AppendText(first + " ");
+                }
+                first--;
+                textBox4.AppendText(Environment.NewLine);
+                for (int j = 0; j < i; j++)
+                {
+                    textBox4.AppendText(second + " ");
+                }
+                second--;
+                textBox4.AppendText(Environment.NewLine);
+            }
+
+        }
+    }
+}
